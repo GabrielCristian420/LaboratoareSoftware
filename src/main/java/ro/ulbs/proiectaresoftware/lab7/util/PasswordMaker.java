9fb66fc73f5317154a772a481dc244594f476f06
GabrielCristian420 <xxgabygamerxxg@gmail.com>
2026-04-21 08:32:57 +0300

    lab7

---
+++ src/main/java/ro/ulbs/proiectaresoftware/lab7/util/PasswordMaker.java
@@ -0,0 +1,35 @@
+package ro.ulbs.proiectaresoftware.lab7.util;
+
+public class PasswordMaker {
+    private static final int MAGIC_NUMBER = 3;
+    private static final String MAGIC_STRING;
+
+    private String name;
+
+    static {
+        StringRandomizer srand = new StringRandomizer();
+        MAGIC_STRING = srand.randomString(20);
+        instance = new PasswordMaker("password");
+    }
+    private static PasswordMaker instance;
+    private static int callingCounts = 0;
+    public static PasswordMaker getInstance() {
+        callingCounts++;
+        return instance;
+    }
+    public static int getCallingCounts() {
+        return callingCounts;
+    }
+    private PasswordMaker(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        java.util.Random r = new java.util.Random();
+        String ln = ""+name.length();
+        ln += r.nextInt(101);
+        StringRandomizer srand = new StringRandomizer();
+        return srand.randomString(MAGIC_NUMBER) + srand.randomString(10, MAGIC_STRING) + ln;
+    }
+}
+
